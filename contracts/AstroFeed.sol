c46852d91feaf300e8a98190fa9e83fe1e717415
Crypto0910 <RoryPorter0910@gmail.com>
2023-03-29 03:04:48 -0400

    Resolved first conflict

---
+++ contracts/AstroFeed.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract AstroFeed is ERC1155, ReentrancyGuard, Ownable {
+
+    uint256 private constant tokenID = 1;
+    mapping(uint256 => string) private tokenURI;
+
+    constructor() ERC1155("https://game.example/api/item/{id}.json")
+    {
+    }
+
+    function mintToken() external nonReentrant onlyOwner{
+        _mint(msg.sender, tokenID, 1, "0x00");
+    }
+
+}
---
+++ hardhat.config.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+require("@nomicfoundation/hardhat-toolbox");
+
+module.exports = {
+  networks: {
+    polygon_mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
